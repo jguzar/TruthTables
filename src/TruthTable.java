3da3a0db7606fd3aaf2a188a5b20ba26e3bb1e57
Jorge A. Guzmán Arellano <jab.guzman@gmail.com>
2015-06-07 00:33:43 -0500

    Creating repo

---
+++ src/TruthTable.java
@@ -0,0 +1,24 @@
+
+public class TruthTable {
+
+	public String formula;
+	private final char[] valid_connectors = {'&','|','>','-'};
+	private final char[] aux_symbols = {'(',')'};
+	
+	public TruthTable(String formula){
+		this.formula = formula;
+		this.processFormula();
+	}
+	
+	private void processFormula(){
+		
+	}
+	
+	public void calculateTable(){
+		
+	}
+	
+	public void printTable(){
+		
+	}
+}
