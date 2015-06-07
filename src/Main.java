514544740aa593f727000bc83b138f3dbc8f5d7d
Jorge A. Guzmán Arellano <jab.guzman@gmail.com>
2015-06-07 18:34:58 -0500

    Imprime tabla de verdad

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String formula = args[0];
-		
-		System.out.print("La fórmula es: ");
-		System.out.println(formula);
-		
+
 		TruthTable table = new TruthTable(formula);
 		table.calculateTable();
 		table.printTable();
--- src/TruthTable.java
+++ src/TruthTable.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,21 +10,31 @@ import java.util.regex.Pattern;
  * */
 public class TruthTable {
 
-	public String formula;
-	private char[] form;;
+	private String formula;
 	private Character[] aux_connectors = { '&', '|', '>', '-' };
 	private Character[] aux_symbols = { '(', ')' };
 	private final ArrayList<Character> valid_connectors = new ArrayList<Character>();
 	private final ArrayList<Character> symbols = new ArrayList<Character>();
 	private ArrayList<Character> variables = new ArrayList<Character>();
-	private HashMap<String, ArrayList> result = new HashMap<String, ArrayList>();
+	private HashMap<String, ArrayList<Integer>> result = new HashMap<String, ArrayList<Integer>>();
 
 	public TruthTable(String formula) {
 		this.formula = formula.trim();
-		this.form = this.formula.toCharArray();
 
+		System.out.print("La fórmula es: ");
 		System.out.println(this.formula);
-		System.out.println(this.form);
+
+		for (int i = 0; i < this.aux_connectors.length; i++)
+			this.valid_connectors.add(this.aux_connectors[i]);
+		for (int i = 0; i < this.aux_symbols.length; i++)
+			this.symbols.add(this.aux_symbols[i]);
+
+		this.setVariables(this.formula);
+		this.processFormula(this.formula);
+	}
+
+	private void processFormula(String formula) {
+		char[] form = formula.toCharArray();
 
 		Pattern p = Pattern
 				.compile("(\\([-]?([A-Z]|[a-z]){1}[&|>]{1}[-]?([A-Z]|[a-z]){1}\\))");
@@ -31,71 +43,63 @@ public class TruthTable {
 		int count = 0;
 		while (m.find()) {
 			count++;
-			
+
 			System.out.println("----------------------------");
 			System.out.println("Match number " + count);
 			System.out.println("Found value: " + m.group(0));
 			int start = m.start();
 			int end = m.end();
 			System.out.println("start(): " + start);
-			System.out.println(this.form[start]);
+			System.out.println(form[start]);
 			System.out.println("end(): " + end);
-			System.out.println(this.form[end-1]);
+			System.out.println(form[end - 1]);
 		}
-
-		for (int i = 0; i < this.aux_connectors.length; i++)
-			this.valid_connectors.add(this.aux_connectors[i]);
-		for (int i = 0; i < this.aux_symbols.length; i++)
-			this.symbols.add(this.aux_symbols[i]);
-		this.getVariables();
-		this.processFormula();
 	}
 
-	private void processFormula() {
-
-	}
-
-	private void getVariables() {
-		ArrayList<ArrayList<Integer>> result_temp = new ArrayList<ArrayList<Integer>>();
-		
-		for (int i = 0; i < this.form.length; i++) {
-			if (!this.valid_connectors.contains(this.form[i])
-					&& !this.symbols.contains(this.form[i])
-					&& !this.variables.contains(this.form[i])){
-				this.variables.add(this.form[i]);
-				//this.result.put(String.valueOf(this.form[i]), new ArrayList<Integer>());
-				result_temp.add(new ArrayList<Integer>());
+	private void setVariables(String formula) {
+		char[] form = formula.toCharArray();
+		for (int i = 0; i < form.length; i++) {
+			if (!this.valid_connectors.contains(form[i])
+					&& !this.symbols.contains(form[i])
+					&& !this.variables.contains(form[i])) {
+				this.variables.add(form[i]);
+				this.result.put(String.valueOf(form[i]),
+						new ArrayList<Integer>());
 			}
 		}
-		
+
 		int[] values = new int[this.variables.size()];
-		for(int i = 0;i<values.length;i++) values[i] = 1;
-		
-		String tmp_size = ""+this.variables.size();
+		for (int i = 0; i < values.length; i++)
+			values[i] = 1;
+
+		String tmp_size = "" + this.variables.size();
 		Double tmp_double_size = Double.parseDouble(tmp_size);
 		int total = (int) Math.pow(2.0, tmp_double_size);
-		System.out.println("Total: "+total);
-		
-		for(int i = 0;i<total;i++){
-			Double k =Double.parseDouble(String.valueOf(this.variables.size()-1));
-			for(int j = 0;j<this.variables.size();j++){
-				result_temp.get(j).add(values[j]);
-				
+
+		for (int i = 0; i < total; i++) {
+			Double k = Double
+					.parseDouble(String.valueOf(this.variables.size() - 1));
+			for (int j = 0; j < this.variables.size(); j++) {
+
+				this.result.get(Character.toString(this.variables.get(j))).add(
+						values[j]);
+
 				int p = (int) Math.pow(2.0, k);
-				int div = Integer.parseInt(new String(""+p));
-				
-				if((i+1)%div==0){
-					if(values[j]==1)
-						values[j]--;
-				}else{
-					if(values[j]==0)
-						values[j]++;
+				int div = Integer.parseInt(new String("" + p));
+
+				if ((i + 1) % div == 0) {
+					if (values[j] == 1)
+						values[j] = 0;
+					else if (values[j] == 0)
+						values[j] = 1;
 				}
 				k--;
 			}
 		}
 
+		System.out.print("Las variables son: ");
 		System.out.println(this.variables);
+		System.out.println(this.result);
 	}
 
 	public void calculateTable() {
@@ -103,6 +107,31 @@ public class TruthTable {
 	}
 
 	public void printTable() {
+		StringBuilder header = new StringBuilder();
+		StringBuilder body = new StringBuilder();
+		ArrayList<String> keys = new ArrayList<String>();
 
+		Iterator it = result.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry pair = (Map.Entry) it.next();
+
+			header.append(pair.getKey());
+			header.append("\t");
+
+			keys.add((String) pair.getKey());
+		}
+		
+		for(int i=0;i<result.get(keys.get(0)).size();i++){
+			
+			for(int j=0;j<keys.size();j++){
+				body.append(result.get(keys.get(j)).get(i));
+				body.append("\t");
+			}
+			body.append("\n");
+		}
+
+		System.out.println("TABLA DE VERDAD:");
+		System.out.println(header);
+		System.out.println(body);
 	}
 }
